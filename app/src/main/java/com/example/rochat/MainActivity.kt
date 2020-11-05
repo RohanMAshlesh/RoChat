7074cf350a16868fc61dcbe322dcf5c0cffed74f
Rohan Manjunath Ashlesh <rashlesh@deloitte.com>
2020-11-05 10:40:29 +0530

    MainActivity definition

--- app/src/main/java/com/example/rochat/MainActivity.kt
+++ app/src/main/java/com/example/rochat/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.rochat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    class PlaceholderFragment: Fragment(){
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val rootView = inflater.inflate(R.layout.fragment_main,container,false)
+
+            return super.onCreateView(inflater, container, savedInstanceState)
+        }
     }
 }
